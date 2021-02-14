e9b4c77ac31e75c14b7ab36c73e529e3a6efbd1f
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-02-14 22:30:01 +0100

    Basic usage + tests

--- src/main/java/PaymentQRCode.java
+++ src/main/java/PaymentQRCode.java
@@ -1,3 +1,81 @@
 public class PaymentQRCode {
 
+    protected final String SEPARATOR = "|";
+
+    protected String recipient;
+    protected String accountNumber;
+    protected double amount;
+    protected String title;
+
+    public PaymentQRCode() {
+    }
+
+    public PaymentQRCode(String recipient, String accountNumber, double amount, String title) {
+        setRecipient(recipient);
+        setAccountNumber(accountNumber);
+        setAmount(amount);
+        setTitle(title);
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getQRCode() {
+        return SEPARATOR + SEPARATOR + getAccountNumber() + SEPARATOR + parseAmountToInteger(this.amount) + SEPARATOR + getRecipient() + SEPARATOR + getTitle() + SEPARATOR + SEPARATOR + SEPARATOR;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = normalizeAmount(amount);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    protected String parseAmountToInteger(double amount) {
+
+        StringBuilder parsedAmount;
+
+        if ((int) amount == amount) {
+            parsedAmount = new StringBuilder((int) amount + "00");
+        } else {
+            parsedAmount = new StringBuilder(String.valueOf((int) (amount * 100)));
+        }
+
+        while(parsedAmount.length() < 6) {
+            parsedAmount.insert(0, "0");
+        }
+
+        return parsedAmount.toString();
+    }
+
+    protected double normalizeAmount(double amount){
+        if((int) amount == amount) {
+            return amount;
+        } else {
+            int temp = (int)(amount * 100.0);
+            return ((double) temp) / 100.0;
+        }
+    }
 }
--- src/test/java/PaymentQRCodeTest.java
+++ src/test/java/PaymentQRCodeTest.java
@@ -4,4 +4,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentQRCodeTest {
 
+    @Test
+    void paymentQRCodeTestConstructor(){
+        PaymentQRCode paymentQRCode = new PaymentQRCode(
+                "Jan Kowalski",
+                "96102005401003791781779627",
+                53.50,
+                "Rachunek za obiad"
+        );
+
+        assertEquals("Jan Kowalski", paymentQRCode.getRecipient());
+        assertEquals("96102005401003791781779627", paymentQRCode.getAccountNumber());
+        assertEquals(53.50, paymentQRCode.getAmount());
+        assertEquals("Rachunek za obiad", paymentQRCode.getTitle());
+    }
+
+    @Test
+    void normalizeAmount() {
+        PaymentQRCode integer = new PaymentQRCode();
+        integer.setAmount(53);
+
+        PaymentQRCode float1 = new PaymentQRCode();
+        float1.setAmount(53.50);
+
+        PaymentQRCode float2 = new PaymentQRCode();
+        float2.setAmount(53.50987);
+
+        PaymentQRCode float3 = new PaymentQRCode();
+        float3.setAmount(53.5);
+
+        assertEquals(53, integer.getAmount());
+        assertEquals(53.50, float1.getAmount());
+        assertEquals(53.50, float2.getAmount());
+        assertEquals(53.50, float3.getAmount());
+    }
+
+    @Test
+    void getQRCode() {
+        PaymentQRCode paymentQRCode = new PaymentQRCode(
+                "Jan Kowalski",
+                "96102005401003791781779627",
+                53.50,
+                "Rachunek za obiad"
+        );
+
+        assertEquals("||96102005401003791781779627|005350|Jan Kowalski|Rachunek za obiad|||", paymentQRCode.getQRCode());
+    }
+
+    @Test
+    void parseAmountToInteger() {
+        PaymentQRCode paymentQRCode = new PaymentQRCode();
+
+        double amountInteger1 = 53;
+        double amountInteger2 = 5300;
+        double amountInteger3 = 530000;
+        double amountFloat1 = 53.5;
+        double amountFloat2 = 53.55;
+
+        assertEquals("005300", paymentQRCode.parseAmountToInteger(amountInteger1));
+        assertEquals("530000", paymentQRCode.parseAmountToInteger(amountInteger2));
+        assertEquals("53000000", paymentQRCode.parseAmountToInteger(amountInteger3));
+        assertEquals("005350", paymentQRCode.parseAmountToInteger(amountFloat1));
+        assertEquals("005355", paymentQRCode.parseAmountToInteger(amountFloat2));
+    }
 }
